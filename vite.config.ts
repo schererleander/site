86adeddfbfd96072cfc24769aa98fc23efe30227
schererleander <leander@schererleander.de>
2025-06-04 01:48:06 +0200

    add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    Sitemap({
+      hostname: "https://schererleander.de",
+      dynamicRoutes: [ "/", "/gear", "/projects", "/homelab", "/printing" ]
+    })
   ],
 })
