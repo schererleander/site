08d6fadccd5b31996c2510481d2af697471a1eb9
schererleander <leander@schererleander.de>
2025-06-25 16:33:24 +0200

    remove unused value

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import CardLink from "../components/CardLink";
 
 interface PostMeta {
--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,4 +1,3 @@
-import { useParams, Link } from "react-router-dom";
 import NotFoundPage from "./404Page";
 
 interface PostFile {
