6d6665eb52d87818df94774e506cf201a8208116
schererleander <leander@schererleander.de>
2025-06-25 02:02:45 +0200

    svg attributes to camelCase

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -27,18 +27,16 @@ export default function Blog() {
   return (
     <section className="container mx-auto px-4 py-10">
       <h1 className="text-4xl font-bold mb-8">Blog</h1>
-
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {posts.map((post) => (
-          <Link key={post.slug} to={`/blog/${post.slug}`} className="block">
-            <CardLink
-              title={post.title}
-              body={post.excerpt}
-              imgSrc={post.cover}
-            />
-          </Link>
-        ))}
-      </div>
+      
+      {posts.map((post) => (
+        <Link key={post.slug} to={`/blog/${post.slug}`} className="block">
+          <CardLink
+            title={post.title}
+            body={post.excerpt}
+            imgSrc={post.cover}
+          />
+        </Link>
+      ))}
     </section>
   );
 }
