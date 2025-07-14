fb039ecfcbcdbd1266b9f48829bb7bef223b4e11
schererleander <leander@schererleander.de>
2025-07-14 18:12:45 +0200

    feat: programming languages/tools

---
+++ src/data/tools.ts
@@ -0,0 +1,56 @@
+export const Tools = [
+  {
+    name: "Linux",
+    color: "bg-neutral-500/20",
+    description: "Server and desktop OS",
+    image: "/icons/linux.svg",
+  },
+  {
+    name: "Git",
+    color: "bg-orange-500/20",
+    description: "Version control",
+    image: "/icons/git.svg",
+  },
+  {
+    name: "Docker",
+    color: "bg-blue-500/20",
+    description: "Containerization",
+    image: "/icons/docker.svg",
+  },
+  {
+    name: "Python",
+    color: "bg-yellow-500/20",
+    description: "Scripting lang",
+    image: "/icons/python.svg",
+  },
+  {
+    name: "Java",
+    color: "bg-orange-500/20",
+    description: "Object-oriented programming",
+    image: "/icons/java.svg",
+  },
+  {
+    name: "Tailwind",
+    color: "bg-blue-500/20",
+    description: "CSS framework",
+    image: "/icons/tailwind.svg",
+  },
+  {
+    name: "React",
+    color: "bg-blue-500/20",
+    description: "UI library",
+    image: "/icons/react.svg",
+  },
+  {
+    name: "TypeScript",
+    color: "bg-blue-500/20",
+    description: "JavaScript but better",
+    image: "/icons/typescript.svg",
+  },
+  {
+    name: "MongoDB",
+    color: "bg-green-500/20",
+    description: "Database",
+    image: "/icons/mongodb.svg",
+  },
+];
