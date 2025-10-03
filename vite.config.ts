29e6008cc47fe3b8e408b8411815f1e131b7d191
schererleander <leander@schererleander.de>
2025-10-04 01:24:36 +0200

    feat: update sitemap.xml

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,18 @@ import Sitemap from 'vite-plugin-sitemap';
 import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    Sitemap({
-      hostname: "https://schererleander.de",
-      dynamicRoutes: [ "/", "/gear", "/projects", "/blog" ]
-    }),
-    mdPlugin({mode: [Mode.REACT, Mode.MARKDOWN],}),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [
+		react(),
+		tailwindcss(),
+		Sitemap({
+			hostname: "https://schererleander.de",
+			dynamicRoutes: ["/", "/blog"]
+		}),
+		mdPlugin({ mode: [Mode.REACT, Mode.MARKDOWN], }),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 })
